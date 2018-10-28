3f16e8a3a6c91a8ff6af71a5f52da7a901040a0b
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-10-28 18:55:29 +0300

    Changed HashRouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import AppContainer from './containers/App';
 import { Provider } from 'react-redux';
