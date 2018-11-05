69e759373c3c8743e61c00b5b8dd632d35b1c272
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-11-05 21:43:29 +0300

    Added recent activity component

---
+++ src/pages/HOME_PAGE/RecentActivity/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {
+  Card,
+} from 'semantic-ui-react';
+
+export default ({ title }) => (
+  <Card.Header>{title}</Card.Header>
+);
--- src/pages/HOME_PAGE/index.js
+++ src/pages/HOME_PAGE/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import RecentActivity from './RecentActivity';
+
+const RecentActivityMock = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/60?random',
+    date: '2018-11-03T18:31:21Z',
+    summary: 'Over speed',
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/60?random',
+    date: '2018-11-04T17:31:21Z',
+    summary: 'Ignore pedestrian',
+  },
+];
 
 export default () => (
-  <p>HOME PAGE</p>
+  <main>
+    <RecentActivity data={RecentActivityMock}/>
+  </main>
 );
